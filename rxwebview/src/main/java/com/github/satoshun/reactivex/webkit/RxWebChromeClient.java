c62ded95d4cd547258e744fb3cc29b6ee0a4ac31
SatoShun <shun.sato1@gmail.com>
2018-06-19 23:28:02 +0000

    add javadoc

--- rxwebview/src/main/java/com/github/satoshun/reactivex/webkit/RxWebChromeClient.java
+++ rxwebview/src/main/java/com/github/satoshun/reactivex/webkit/RxWebChromeClient.java
@@ -1,19 +1,22 @@
 package com.github.satoshun.reactivex.webkit;
 
+import com.github.satoshun.reactivex.webkit.data.RxWebChromeClientData;
+import com.github.satoshun.reactivex.webkit.internal.ObjectHelper;
+
 import android.support.annotation.CheckResult;
 import android.support.annotation.NonNull;
 import android.webkit.WebView;
 
-import com.github.satoshun.reactivex.webkit.data.RxWebChromeClientData;
-import com.github.satoshun.reactivex.webkit.internal.ObjectHelper;
-
 import io.reactivex.Observable;
 
 /**
- * static factory methods for creating {@linkplain io.reactivex.Completable}
- * and {@linkplain io.reactivex.Observable}
+ * static factory methods for creating {@linkplain io.reactivex.Observable}
  */
 public class RxWebChromeClient {
+
+  /**
+   * Create an observable of WebView from WebChromeClient.
+   */
   @CheckResult @NonNull
   public static Observable<RxWebChromeClientData> events(@NonNull WebView webView) {
     ObjectHelper.requireNonNull(webView, "webView is null");
