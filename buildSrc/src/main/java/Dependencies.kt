e7aa829db425f9bb66e293a061478064d22c0b7c
SatoShun <shun.sato1@gmail.com>
2018-06-19 08:35:43 +0000

    update agp, kotlin

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,9 +2,9 @@ object Vers {
   val compile_sdk = 27
   val min_sdk = 14
   val target_sdk = 27
-  val agp = "3.1.2"
+  val agp = "3.1.3"
 
-  val kotlin = "1.2.41"
+  val kotlin = "1.2.50"
   val support_lib = "27.1.1"
 }
 
@@ -14,7 +14,7 @@ object Libs {
   val dokka_plugin = "org.jetbrains.dokka:dokka-android-gradle-plugin:0.9.16"
 
   val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:${Vers.kotlin}"
-  val rx_java = "io.reactivex.rxjava2:rxjava:2.1.8"
+  val rx_java = "io.reactivex.rxjava2:rxjava:2.1.14"
   val rx_android = "io.reactivex.rxjava2:rxandroid:2.0.1"
 
   val appcompat_v7 = "com.android.support:appcompat-v7:${Vers.support_lib}"
