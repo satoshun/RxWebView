8647724e5ceaf9aa95862ca55815bb9f187ca7ef
SatoShun <shun.sato1@gmail.com>
2018-10-26 02:40:35 +0000

    update test libs

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -21,9 +21,9 @@ object Libs {
   val android_annotation = "androidx.annotation:annotation:1.0.0"
 
   val junit = "junit:junit:4.12"
-  val test_runner = "androidx.test:runner:1.1.0-beta02"
-  val test_rule = "androidx.test:rules:1.1.0-beta02"
-  val espresso = "androidx.test.espresso:espresso-core:3.1.0-beta02"
+  val test_runner = "androidx.test:runner:1.1.0"
+  val test_rule = "androidx.test:rules:1.1.0"
+  val espresso = "androidx.test.espresso:espresso-core:3.1.0"
 
   val truth = "com.google.truth:truth:0.42"
   val mockito_kotlin = "com.nhaarman:mockito-kotlin-kt1.1:1.5.0"
