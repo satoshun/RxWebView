6c3e92b3db30f94e3d7ad08b7f5eec687ae852c5
SatoShun <shun.sato1@gmail.com>
2018-10-29 23:11:48 +0000

    RxWebViewClientTest convert to kotlin

---
+++ rxwebview/src/androidTest/java/com/github/satoshun/reactivex/webkit/RxWebViewClientTest.kt
@@ -0,0 +1,35 @@
+package com.github.satoshun.reactivex.webkit
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import com.github.satoshun.reactivex.webkit.data.OnPageStarted
+import io.reactivex.android.schedulers.AndroidSchedulers
+import org.hamcrest.Matchers.`is`
+import org.junit.Assert.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+internal class RxWebViewClientTest {
+  @get:Rule val activityRule = ActivityTestRule(RxWebViewTestActivity::class.java)
+
+  private val activity: RxWebViewTestActivity get() = activityRule.activity
+
+  @Test
+  fun events() {
+    val data = RxWebViewClient.events(activity.webview)
+        .ofType(OnPageStarted::class.java)
+        .take(1)
+        .doOnSubscribe {
+          activity.webview.loadUrl("https://www.google.com/")
+        }
+        .subscribeOn(AndroidSchedulers.mainThread())
+        .test()
+        .await()
+        .values()[0]
+    assertThat(data.url, `is`("https://www.google.com/"))
+  }
+}
